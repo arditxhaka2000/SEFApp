3cf988b58addf6c2338671491b4ef6d09994d407
arditxhaka2000 <arditxhaka2000@gmail.com>
2025-06-25 13:48:10 +0200

    Add project files.

---
+++ SEFApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SEFApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SEFApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SEFApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
